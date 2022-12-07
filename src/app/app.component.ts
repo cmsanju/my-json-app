3ce440b6e708b726b3056a7722f30e72c349c6c0
cmsanju <lict.sanju@gmail.com>
2022-12-07 15:03:39 +0530

    json table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import studentsData from './students.json';  
+    
+interface Student {  
+    id: Number;  
+    name: String;  
+    email: String;  
+    gender: String;  
+}  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-json-app';
+
+  students: Student[] = studentsData;  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppComponent } from './app.component';
 
@@ -8,8 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    DataTablesModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
